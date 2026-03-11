f0a5275dbfd00b4fcc2612fd8426563ce7128834
Utku Cüre <rednbesoftware@gmail.com>
2026-03-11 11:31:31 +0300

    Add ChatWindow (Not working for now)

---
+++ Views/ChatWindow.axaml.cs
@@ -0,0 +1,15 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using DesktopPet.Classes;
+
+namespace DesktopPet.Views;
+
+public partial class ChatWindow : Window
+{
+    public ChatWindow()
+    {
+        //InitializeComponent();
+    }
+}
